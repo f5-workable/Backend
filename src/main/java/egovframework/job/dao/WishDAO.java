ce02ad09b9ba4a392bb6d36b0295a8c29d60d372
HongSungYun <foodb244@gmail.com>
2023-05-19 17:17:28 +0900

    회원별 찜목록 조회, 찜 등록, 찜 해제 기능 추가

--- src/main/java/egovframework/job/dao/WishDAO.java
+++ src/main/java/egovframework/job/dao/WishDAO.java
@@ -6,7 +6,8 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import egovframework.job.vo.WishVO;
+import egovframework.job.vo.JobinfoVO;
+import egovframework.job.vo.WishCreateVO;
 
 @Repository
 public class WishDAO {
@@ -14,9 +15,16 @@ public class WishDAO {
 	@Autowired
 	private SqlSession sqlSession;
 //	로그인한 회원의 찜목록을 가져올때
-//	public List<WishVO> selectWish() {
-//		List<WishVO> res = sqlSession.selectList("egovframework.mapper.job.WishMapper.", member)
-//		return res;
-//	}
-	
+	public List<JobinfoVO> selectWishList(Long memberId) {
+		List<JobinfoVO> res = sqlSession.selectList("egovframework.mapper.job.WishMapper.selectWishList", memberId);
+		return res;
+	}
+//	찜클릭시 생성
+	public void createWish(WishCreateVO vo) {
+		sqlSession.insert("egovframework.mapper.job.WishMapper.createWish", vo);
+	}
+//	찜 해제
+	public void deleteWish(WishCreateVO vo) {
+		sqlSession.delete("egovframework.mapper.job.WishMapper.deleteWish", vo);
+	}
 }
