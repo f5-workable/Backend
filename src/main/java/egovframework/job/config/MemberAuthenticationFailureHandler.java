75ca0105f1d051f15e841d0f91a00c2fe3994d3b
Ha Su Min <hasm0326@naver.com>
2023-05-25 15:00:45 +0900

    멤버 비밀번호 찾기, 변경 추가

--- src/main/java/egovframework/job/config/MemberAuthenticationFailureHandler.java
+++ src/main/java/egovframework/job/config/MemberAuthenticationFailureHandler.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 
-public class CustomAuthenticationFailureHandler implements AuthenticationFailureHandler {
+public class MemberAuthenticationFailureHandler implements AuthenticationFailureHandler {
 
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
--- src/main/java/egovframework/job/dao/MemberDAO.java
+++ src/main/java/egovframework/job/dao/MemberDAO.java
@@ -13,11 +13,6 @@ public class MemberDAO extends EgovAbstractMapper {
 		insert("egovframework.mapper.job.MemberMapper.registerMember", memberDTO);
 	}
 
-	// 로그인 처리
-	public MemberDTO actionLogin(MemberDTO memberDTO) throws Exception {
-		return (MemberDTO) selectOne("egovframework.mapper.job.MemberMapper.actionLogin", memberDTO);
-	}
-
 	// 아이디 검색
 	public MemberDTO findById(String id) throws Exception {
 		return (MemberDTO) selectOne("egovframework.mapper.job.MemberMapper.findById", id);
@@ -27,6 +22,20 @@ public class MemberDAO extends EgovAbstractMapper {
 	public MemberDTO getMemberDetail(String id) throws Exception {
 		return (MemberDTO) selectOne("egovframework.mapper.job.MemberMapper.findById", id);
 	}
+	
+	// 비밀번호 찾기
+	public String findPassword(String id, String name, String phone) throws Exception {
+	    MemberDTO memberDTO = new MemberDTO();
+	    memberDTO.setId(id);
+	    memberDTO.setName(name);
+	    memberDTO.setPhone(phone);
+	    return (String) selectOne("egovframework.mapper.job.MemberMapper.findPassword", memberDTO);
+	}
+	
+	// 비밀번호 변경 처리
+    public void updatePassword(MemberDTO memberDTO) throws Exception {
+        update("egovframework.mapper.job.MemberMapper.updatePassword", memberDTO);
+    }
 
 	// 아이디 상세정보 수정
 	public void updateMemberDetail(MemberDTO memberDTO) throws Exception {
